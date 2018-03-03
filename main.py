32658c4e6c53226f854727ebda9a0821d1faad00
osjia <19olgakryukova@gmail.com>
2018-03-03 17:08:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
+# -*- coding: utf-8 -*- 
+
+import random
+
 import requests
+
+def point(event, context):
+    print(event)
+    if event["message"]["text"][0] == "/":
+        words = event["message"]["text"].split()
+        command = words[0][1:]
+        if command == "echo":
+            send_message(event["message"]["from"]["id"], event["message"]["text"])
+        elif command == "help":
+            help_text = "Это вспомогательный текст. Здесь будет написано что и как со мной можно сделать"
+            send_message(event["message"]["from"]["id"], help_text)
+        elif command == "joke":
+            JOKES_DICT = [
+                "Шутка1",
+                "Шутка2",
+                "Шутка3"
+            ]
+            send_message(event["message"]["from"]["id"], random.choice(JOKES_DICT))
+        else:
+            send_message(event["message"]["from"]["id"], "Я не знаю эту команду")
+    else:
+        send_message(event["message"]["from"]["id"], "Введите команду")
+    
+
+def send_message(chat_id, text):
+    url = "https://api.telegram.org/bot{token}/{method}".format(
+        token="569498492:AAEvy6yRzqkygb9DXpnPTOICbihG-rkq6rc",
+        method="sendMessage"
+    )
+    data = {
+        "chat_id": chat_id,
+        "text": text
+    }
+    r = requests.post(url, data = data)
+    print(r.json())
+
+
+
+"""import requests
 import datetime
 
 token = '569498492:AAEvy6yRzqkygb9DXpnPTOICbihG-rkq6rc'
@@ -10,7 +53,7 @@ class BotHandler:
         self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
-    def get_updates(self, offset=None, timeout=60):
+    def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
@@ -72,3 +115,4 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         exit()
+"""
